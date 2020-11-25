197be459e9e076d303234620b7309b6875772301
Eduard Moraru <Enygma2002@gmail.com>
2020-11-25 19:46:29 +0200

    [build] Added easy to use release script.

---
+++ release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PARAMETERS="-DskipTests=true -Dxwiki.enforcer.skip=true"
+
+mvn release:prepare -U ${PARAMETERS} -DautoVersionSubmodules=true -Darguments="${PARAMETERS}" && mvn release:perform ${PARAMETERS} -Darguments="${PARAMETERS}"
+ 
